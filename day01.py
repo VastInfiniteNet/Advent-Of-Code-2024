1dc1de1a2d68797646d072f274bc0143aeff3422
ÆMAETH <screwthisusernameprocess@gmail.com>
2024-12-02 16:43:53 -0500

    cleanup

--- day01.py
+++ day01.py
@@ -1,44 +1,23 @@
 # https://adventofcode.com/2024/day/1
 
 def part1(lefts, rights):
-    total = 0
-    leftNums = sorted(lefts)
-    rightNums = sorted(rights)
-    for i in range(len(leftNums)):
-        total += abs(int(leftNums[i]) - int(rightNums[i]))
-    return total
+    return sum([abs(int(i) - int(j)) for i,j in zip(sorted(lefts), sorted(rights))]) # diff sum
 
 def part2(lefts, rights):
-    leftCounts, rightCounts, total = {}, {}, 0
+    leftCounts, rightCounts = {}, {}
     for i in range(len(lefts)):
         leftNum, rightNum = lefts[i], rights[i]
-        if leftNum in leftCounts:
-            leftCounts[leftNum] += 1
-        else:
-            leftCounts[leftNum] = 1
-        if rightNum in rightCounts:
-            rightCounts[rightNum] += 1
-        else:
-            rightCounts[rightNum] = 1
-    for n in list(leftCounts):
-        if n in rightCounts:
-            total += int(n) * leftCounts[n] * rightCounts[n]
-    return total
+        leftCounts[leftNum] = leftCounts.get(leftNum, 0) + 1
+        rightCounts[rightNum] = rightCounts.get(rightNum, 0) + 1
+    return sum([int(n) * leftCounts.get(n,0) * rightCounts.get(n,0) for n in list(leftCounts)])
 
 def processInput():
-    left, right = [], []
     with open("day01.txt") as file:
-        for line in file:
-            leftString, rightString = line.strip().split()
-            left.append(leftString)
-            right.append(rightString)
-    return (left, right)
+        return list(zip(*[line.strip().split() for line in file]))
 
 def main():
     inputs = processInput()
-
     print(f"Part one answer: {part1(*inputs)}")
     print(f"Part two answer: {part2(*inputs)}")
-
 
 main()
--- day02.py
+++ day02.py
@@ -1,42 +1,27 @@
 # https://adventofcode.com/2024/day/2
 
-def validateReport(report, allowedMistakes = False):
+def validateReport(report, allowedMistakes):
     startDirection = -1 if report[1] - report[0] < 0 else 1
     for i in range(len(report) - 1):
         levelDiff = report[i+1] - report[i]
         currDirection = -1 if levelDiff < 0 else 1
-        if (
-            startDirection != currDirection or
-            abs(levelDiff) > 3 or 
-            abs(levelDiff) == 0
-        ):
-            if allowedMistakes: # some error in a 3 wide problem window X Y Y Y X
-                                # try to remove a level and retest
-                return  (   validateReport(report[0:i-1] + report[i:], False) or    # remove left side of window
-                            validateReport(report[0:i] + report[i+1:], False) or    # remove center of window
-                            validateReport(report[0:i+1] + report[i+2:], False))    # remove right side of window
-            else:
+        if startDirection != currDirection or abs(levelDiff) > 3 or abs(levelDiff) == 0:
+            if not allowedMistakes:
                 return False
+            # some error in a 3 wide problem window X Y Y Y X, remove a level and retest
+            return (validateReport(report[0:i-1] + report[i:], False) or    # remove left side of window
+                    validateReport(report[0:i] + report[i+1:], False) or    # remove center of window
+                    validateReport(report[0:i+1] + report[i+2:], False))    # remove right side of window
     return True
 
-def countReports(reports, allowedMistake):
-    safeCount = 0
-    for report in reports:
-        if validateReport(report, allowedMistake):
-                safeCount += 1
-    return safeCount
-
 def processInput():
-    reports = []
     with open("day02.txt") as file:
-        for line in file:
-            reports.append(list(map(int, line.strip().split())))
-    return reports
+        return [list(map(int, line.strip().split())) for line in file]
 
 def main():
     reports = processInput()
-
-    print(f"Part one answer: {countReports(reports, False)}")
-    print(f"Part two answer: {countReports(reports, True)}")
+    countReports = lambda m: sum([validateReport(r, m) for r in reports])
+    print(f"Part one answer: {countReports(False)}")
+    print(f"Part two answer: {countReports(True)}")
 
 main()
